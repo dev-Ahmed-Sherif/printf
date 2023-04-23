618ae1238c55354624ac02ce42bdf519f7a1ace3
dev-Ahmed-Sherif <ahmedsherif91101@gmail.com>
2023-04-22 23:47:44 -0700

    add main.h

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+
+#define NULL_STRING "(null)"
+#define NUL '\0'
+
+/**
+ * struct convert - defines a structure for symbols and functions
+ *
+ * @sym: The operator
+ * @f: The function associated
+ */
+
+int _printf(const char *format, ...);
+#endif
